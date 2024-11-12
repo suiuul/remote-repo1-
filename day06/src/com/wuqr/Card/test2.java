d6d7365d7b844d02a01257c345ceb67bb117359b
suiuul <928378107@qq.com>
2024-11-12 21:59:58 +0800

    commit test2 2

--- day06/src/com/wuqr/Card/test2.java
+++ day06/src/com/wuqr/Card/test2.java
@@ -2,6 +2,6 @@ package com.wuqr.Card;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println(git test2);
+        System.out.println("git test2");
     }
 }
